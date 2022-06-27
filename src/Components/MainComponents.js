bfd4284271f4e9b83cc82969dd494c9fbc733e2e
Abdullah Al Hadi <abdullahalhadi317@gmail.com>
2022-06-27 16:42:09 +0600

    work update

---
+++ src/Components/MainComponents.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Body from './body/Body';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+
+
+const MainComponents = () => {
+    return (
+        <div>
+            <Body />
+            <Header />
+            <Footer />
+        </div>
+    );
+};
+
+export default MainComponents;
